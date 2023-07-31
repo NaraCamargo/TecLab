5bda06add9e806c3e5945e39403c1a3b1647a74d
NaraCamargo <naraliggia2@gmail.com>
2023-07-31 12:30:36 -0300

    31/07

--- src/main/java/br/com/tcc/teclab/repository/AgendamentoRepository.java
+++ src/main/java/br/com/tcc/teclab/repository/AgendamentoRepository.java
@@ -1,4 +1,7 @@
 package br.com.tcc.teclab.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface AgendamentoRepository {
 }
--- src/main/java/br/com/tcc/teclab/repository/TurmasRepository.java
+++ src/main/java/br/com/tcc/teclab/repository/TurmasRepository.java
@@ -1,4 +1,12 @@
 package br.com.tcc.teclab.repository;
 
-public interface TurmasRepository {
+import br.com.tcc.teclab.model.Turmas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TurmasRepository extends JpaRepository<Turmas, Integer> {
+
+
+
 }
